a5497b568d53453147b3b195a82aa644e3d8558f
thiele67 <>
2021-05-29 11:34:03 +0200

    added basic occlusions

--- hylite/correct/illumination/occlusion.py
+++ hylite/correct/illumination/occlusion.py
@@ -10,6 +10,7 @@ from abc import ABCMeta, abstractmethod
 import numpy as np
 import hylite
 from hylite.correct.illumination.source import SourceModel
+from hylite.analyse import band_ratio
 
 class OccModel(object):
     """
@@ -49,16 +50,17 @@ class OccModel(object):
             xyz = self.geometry.get_xyz().reshape((shape[0] * shape[1], 3)).copy()
             klm = self.geometry.get_normals().reshape((shape[0] * shape[1], 3)).copy()
             rad = self.geometry.image.data.reshape((shape[0] * shape[1], -1)).copy()
-            self.occ = hylite.HyImage(self.compute(xyz, klm, **kwds).reshape(shape + (1,)))
+            rad = hylite.HyData(rad)
+            rad.set_wavelengths(image.get_wavelengths())
+            self.occ = hylite.HyImage(self.compute(xyz, klm, rad, **kwds).reshape(shape + (1,)))
 
         # apply to HyCloud data
         elif isinstance(self.geometry, hylite.HyCloud):
             xyz = self.geometry.xyz.copy()
             klm = self.geometry.normals.copy()
             rad = self.geometry.copy()
-
             self.occ = self.geometry.cloud.copy()
-            self.occ.data = self.compute(xyz, klm, **kwds)[..., 0]
+            self.occ.data = self.compute(xyz, klm, rad, **kwds)[..., 0]
         else:
             assert False, "Error, self.geometry must be an instance of HyScene or HyCloud."
 
@@ -66,7 +68,7 @@ class OccModel(object):
         return self.occ
 
     @abstractmethod
-    def compute(self, xyz, klm, **kwds):
+    def compute(self, xyz, klm, rad, **kwds):
         """
         *Arguments*:
          - xyz = a (n,3) array of point or pixel coordinates in 3-D space.
@@ -98,7 +100,7 @@ class OccModel(object):
                 assert False, "Error: call evaluate(...) first to compute occlusions before plotting."
 
         if isinstance(self.geometry, hylite.HyCloud):
-            cam = kwds.get("cam", self.geometry.get_camera(0))
+            cam = kwds.get("cam", scene.geometry.get_camera(0))
             return self.occ.quick_plot(0, cam)
         else:
             return self.occ.quick_plot(0)
@@ -109,9 +111,34 @@ class selfOcc(OccModel):
     Model self occlusions using a band ratio.
     """
 
-    def compute(self, xyz, klm, **kwds):
+    def compute(self, xyz, klm, rad, **kwds):
         """
         Calculate self-occlusions using the angle of incidience (occlude if > 90 deg).
         """
-        a = np.dot(klm, -self.source.illuVec)  # calculate angle of incidence
-        return (a > 0).astype(np.float)  # return True for self-occ areas
+        a = np.dot(klm, -source.illuVec)  # calculate angle of incidence
+        return (a < 0).astype(np.float)  # return True for self-occ areas
+
+
+class bandOcc(OccModel):
+    """
+    Model occlusion using a band ratio to identify shadows that are significantly "bluer" than their surroundings.
+    """
+
+    def compute(self, xyz, klm, rad, **kwds):
+        """
+        Calculate the fraction of incoming light blocked by this occlusion (0 - 1) as a HyData instance.
+
+        *Keywords*:
+         - radiance = Radiance data to calculate band ratio with.
+         - num = Numerator of band ratio, as defined by hylite.analyse.band_ratio. Default is (400,450) nm.
+         - den = Denominator of band ratio, as defined by hylite.analyse.band_ratio. Default is (550,600) nm.
+         - a = the threshold for complete shadow (values > this are full shadow). Default is 1.20.
+         - b = the threshold for complete sun (values < this are full sun). Default is 1.3.
+        *Returns*:
+         - A HyCloud instance containing the occlusion factor (0-1) for each point.
+        """
+        br = band_ratio(rad, kwds.get('num', (400., 450.)), kwds.get('num', (550., 600.)))
+        out = br.data[..., 0] - kwds.get('a', 1.20)
+        out /= kwds.get('b', 1.3) - kwds.get('a', 1.2)
+        out = np.clip(out, 0, 1)
+        return out
