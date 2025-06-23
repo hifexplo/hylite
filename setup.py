f46b6fb146c0affaa4f01ee83162bef19c2398ca
samthiele <sam.thiele01@gmail.com>
2025-06-23 10:41:17 +0200

    increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hylite",
-    version="1.35",
+    version="1.36",
     author="Helmholtz Institute Freiberg",
     author_email="s.thiele@hzdr.de",
     description="Open-source toolbox for hyperspectral geology.",
